cb34ec204b4713d6b8cff1801129b7908ba57147
Nicolas BENIZRI <20011494>
2022-12-01 16:08:42 +0100

    clean day1

--- src/main/java/com/nbi/adventofcode2022/day1/Day1Resolver.java
+++ src/main/java/com/nbi/adventofcode2022/day1/Day1Resolver.java
@@ -4,49 +4,51 @@ import com.nbi.adventofcode2022.utils.Utils;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Day1Resolver {
 
-
     public Integer resolveDay1Step1(String inputStep1) {
-        List<QuantityByElf> quantityByElves = getQuantityByElves(inputStep1);
-
-        return quantityByElves.get(0).quantity;
+        return getSummedQuantity(inputStep1, 1);
     }
 
     public Integer resolveDay1Step2(String inputStep1) {
-        List<QuantityByElf> quantityByElves = getQuantityByElves(inputStep1);
+        return getSummedQuantity(inputStep1, 3);
+    }
 
-        return quantityByElves.get(0).quantity + quantityByElves.get(1).quantity + quantityByElves.get(2).quantity;
+    private Integer getSummedQuantity(String inputStep1, int maxSize) {
+        List<QuantityByElf> quantityByElves = getQuantityByElves(inputStep1);
+        return quantityByElves
+                .stream()
+                .limit(maxSize)
+                .map(QuantityByElf::getQuantity)
+                .reduce(Integer::sum)
+                .orElse(0);
     }
 
     private List<QuantityByElf> getQuantityByElves(String inputStep1) {
-        List<String> lines = Utils.getLines(inputStep1);
+        List<String> lines = new ArrayList<>();
+        lines.add("");
+        lines.addAll(Utils.getLines(inputStep1));
+
         List<QuantityByElf> quantityByElves = new ArrayList<>();
         int elfIdentifier = 0;
-        boolean first = true;
         QuantityByElf current = null;
+
         for (String line : lines) {
-            if (first) {
-                current = new QuantityByElf(elfIdentifier++, 0);
-                quantityByElves.add(current);
-            }
             if (!StringUtils.hasLength(line)) {
                 current = new QuantityByElf(elfIdentifier++, 0);
                 quantityByElves.add(current);
                 continue;
             }
             current.setQuantity(current.getQuantity() + Integer.valueOf(line));
-            first = false;
         }
 
         quantityByElves.sort((o1, o2) -> o2.getQuantity().compareTo(o1.getQuantity()));
         return quantityByElves;
     }
 
-    class QuantityByElf {
+    static class QuantityByElf {
         Integer elfIdentifier;
         Integer quantity;
 
